3b9f23998e953be11a6926e4a6a2f70b56c8e7dc
NK <45469562+navapro@users.noreply.github.com>
2020-07-20 15:12:29 -0600

    Create App.js

---
+++ covid-19-tracker/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Cards from './Components/Cards/Cards';
+import Charts from './Components/Charts/Charts';
+import CountryPicker from './Components/CountryPicker/CountryPicker';
+
+class App extends React.Component {
+    render(){
+        return(
+            <div>
+                <h1>App</h1>
+            </div>
+        );
+    }
+}
+
+export default App ;
