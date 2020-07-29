58db43c120058e3d36d392c3a3e7439ad7fc301a
NK <45469562+navapro@users.noreply.github.com>
2020-07-29 17:36:59 -0600

    Makea API call

--- covid-19-tracker/src/App.js
+++ covid-19-tracker/src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 
 import {Cards, Charts, CountryPicker} from "./components";
+import fetchData from './api';
 
 import styles from './App.module.css';
 
 class App extends React.Component {
+
+    async ComponentDidMount(){
+        const data = await fetchData();
+
+        console.log(data)
+    }
     render(){
         return(
             <div className ={styles.container}>
