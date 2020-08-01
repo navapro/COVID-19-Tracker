ba8fe05fcbc5a4f39acf4d74cb25fdd69a00dab5
NK <45469562+navapro@users.noreply.github.com>
2020-08-01 09:49:42 -0600

    Pass api data to cards

--- covid-19-tracker/src/App.js
+++ covid-19-tracker/src/App.js
@@ -7,15 +7,20 @@ import styles from './App.module.css';
 
 class App extends React.Component {
 
-    async componentDidMount(){
-        const data = await fetchData();
+    state= {
+        data = {},
+    }
 
-        console.log(data)
+    async componentDidMount(){
+        const fetchedData = await fetchData();
+        this.setState({data:fetchedData});
     }
     render(){
+        const {data} = this.state;
+
         return(
             <div className ={styles.container}>
-                <Cards/>
+                <Cards data = {data}/>
                 <CountryPicker/>
                 <Charts/>
             </div>
