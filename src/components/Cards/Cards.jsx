be5d1f94ba2fb17c2d56c8dc4719943339009d0e
NK <45469562+navapro@users.noreply.github.com>
2020-08-01 10:35:40 -0600

    Update Cards.jsx

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 
+import { Card, CardContent, Typography, Grid } from '@material-ui/core' ;
+import styles from './Cards.module.css';
+
 const Cards = (props) => { 
     console.log(props)
     return(
-        <h1>Cards</h1>
+        <div className = {styles.container}>
+            <Grid contaner spacing = {3} justify = "center">
+                <Grid item component = {Card} >
+                    
+                </Grid>
+            </Grid>
+        </div>
     );
 }
 
