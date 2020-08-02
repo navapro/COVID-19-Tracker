4120d7c8ce07666a1fd24eafe90569d37b4f25fd
NK <45469562+navapro@users.noreply.github.com>
2020-08-02 11:04:04 -0600

    material UI components for cards
    Integrated Material UI for cards

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -3,13 +3,18 @@ import React from 'react';
 import { Card, CardContent, Typography, Grid } from '@material-ui/core' ;
 import styles from './Cards.module.css';
 
-const Cards = (props) => { 
+const Cards = (props) => {
     console.log(props)
     return(
         <div className = {styles.container}>
             <Grid contaner spacing = {3} justify = "center">
                 <Grid item component = {Card} >
-                    
+                    <CardContent> 
+                        <Typography color = "textDecodary"> Infected</Typography>
+                        <Typography varaint = "h5"> REAL DATA </Typography>
+                        <Typography color = "textSecondary">DATE</Typography>
+                        <Typography varaint = "body2"> # of active cases</Typography>
+                    </CardContent>
                 </Grid>
             </Grid>
         </div>
