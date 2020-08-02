5be0dd94c32cafd2b7cf95482a86a8f13ce727ac
NK <45469562+navapro@users.noreply.github.com>
2020-08-02 11:34:00 -0600

    Display number of confirmed cases

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,40 +1,51 @@
-import React from 'react';
+import React from "react";
 
-import { Card, CardContent, Typography, Grid } from '@material-ui/core' ;
-import styles from './Cards.module.css';
+import { Card, CardContent, Typography, Grid } from "@material-ui/core";
+import styles from "./Cards.module.css";
+import CountUp from "react-countup";
 
-const Cards = (props) => {
-    console.log(props)
-    return(
-        <div className = {styles.container}>
-            <Grid contaner spacing = {3} justify = "center">
-                <Grid item component = {Card} >
-                    <CardContent> 
-                        <Typography color = "textDecodary">Infected</Typography>
-                        <Typography varaint = "h5">REAL DATA</Typography>
-                        <Typography color = "textSecondary">DATE</Typography>
-                        <Typography varaint = "body2">Number of Active COVID-19 cases</Typography>
-                    </CardContent>
-                </Grid>
-                <Grid item component = {Card} >
-                    <CardContent> 
-                        <Typography color = "textDecodary">Recovered</Typography>
-                        <Typography varaint = "h5">REAL DATA</Typography>
-                        <Typography color = "textSecondary">DATE</Typography>
-                        <Typography varaint = "body2">Number of recovereies from COVID-19</Typography>
-                    </CardContent>
-                </Grid>
-                <Grid item component = {Card} >
-                    <CardContent> 
-                        <Typography color = "textDecodary">Deaths</Typography>
-                        <Typography varaint = "h5">REAL DATA</Typography>
-                        <Typography color = "textSecondary">DATE</Typography>
-                        <Typography varaint = "body2">Number of deaths caused by COVID-19</Typography>
-                    </CardContent>
-                </Grid>
-            </Grid>
-        </div>
-    );
-}
+const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
+  if (!confirmed) {
+    return "Loading....";
+  }
+  return (
+    <div className={styles.container}>
+      <Grid contaner spacing={3} justify="center">
+        <Grid item component={Card}>
+          <CardContent>
+            <Typography color="textDecodary">Infected</Typography>
+            <Typography varaint="h5">
+              <CountUp start={0} end={confirmed.value} duration={2.5} separator=","/>
+            </Typography>
+            <Typography color="textSecondary">DATE</Typography>
+            <Typography varaint="body2">
+              Number of Active COVID-19 cases
+            </Typography>
+          </CardContent>
+        </Grid>
+        <Grid item component={Card}>
+          <CardContent>
+            <Typography color="textDecodary">Recovered</Typography>
+            <Typography varaint="h5">REAL DATA</Typography>
+            <Typography color="textSecondary">DATE</Typography>
+            <Typography varaint="body2">
+              Number of recovereies from COVID-19
+            </Typography>
+          </CardContent>
+        </Grid>
+        <Grid item component={Card}>
+          <CardContent>
+            <Typography color="textDecodary">Deaths</Typography>
+            <Typography varaint="h5">REAL DATA</Typography>
+            <Typography color="textSecondary">DATE</Typography>
+            <Typography varaint="body2">
+              Number of deaths caused by COVID-19
+            </Typography>
+          </CardContent>
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
 
 export default Cards;
