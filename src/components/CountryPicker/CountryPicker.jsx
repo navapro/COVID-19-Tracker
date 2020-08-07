6519342251bdbac0d37aaf16576b2b77562faa95
NK <45469562+navapro@users.noreply.github.com>
2020-08-07 13:45:57 -0600

    Update CountryPicker.jsx

--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import { NativeSelect, FormControl } from '@material-ui/core';
+import styles from './CountryPicker.module.css';
+import {countries} from '../../api';
 
 const CountryPicker = () => { 
     return(
-        <h1>CountryPicker</h1>
+        <FormControl className = {styles.FormControl}>
+            <NativeSelect>
+                <options value = "global"> Global </options>
+            </NativeSelect>
+        </FormControl>
     );
 }
 
